cec79fb76e332ca7cb0b7612126a65f24651ac3f
elgamal <ahmedelgamal5320@gmail.com>
2024-10-23 08:28:31 +0300

    feat: send the first email

---
+++ resources/views/mails/first-mail.blade.php
@@ -0,0 +1,3 @@
+<h1 class="">
+    This is my first email ever sent by laravel
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Mail\FirstMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::get('/send', function () {
+    Mail::to('test@test.com')->send(new FirstMail());
+
+    return to_route('home');
 });
